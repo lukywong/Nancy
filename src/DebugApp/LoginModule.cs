f46769165d87a40d7162f7ac5bf2befc701108ac
Andreas Hakansson <andreas@selfinflicted.org>
2011-11-15 15:00:40 +0100

    When theres something strange in the neighborhood...

---
+++ src/DebugApp/LoginModule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace TryOutNancy
+{
+    public class LoginModule : NancyModule
+    {
+        public LoginModule()
+        {
+
+            Post["/login"] = x =>
+            {
+                LoginInfo login = this.Bind();
+
+                return login.IsValid()
+                    ? "Success"
+                    : "Failure";
+            };
+
+            Get["/"] = x =>
+            {
+                return View["login"];
+            };
+        }
+    }
+
+    public class LoginInfo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public bool IsValid()
+        {
+            return Username == "user" && Password == "pass";
+        }
+    }
+}
